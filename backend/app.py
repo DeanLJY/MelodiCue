4b0f7f653a371b0793424519087a1901a471d29e
Dean Leung <100091478+FalloutOne@users.noreply.github.com>
2022-04-16 18:14:56 +0800

    Add basic queries

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,34 @@
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # Imports
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+from gqlalchemy import Memgraph
+from models import Track, Playlist
 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # App Config.
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-app.config.from_object('config')
-#db = SQLAlchemy(app)
+app.config.from_object("config")
+memgraph = Memgraph()
 
 # Automatically tear down SQLAlchemy.
-'''
+"""
 @app.teardown_request
 def shutdown_session(exception=None):
     db_session.remove()
-'''
+"""
 
 # Login required decorator.
-'''
+"""
 def login_required(test):
     @wraps(test)
     def wrap(*args, **kwargs):
@@ -35,73 +38,102 @@ def login_required(test):
             flash('You need to login first.')
             return redirect(url_for('login'))
     return wrap
-'''
-#----------------------------------------------------------------------------#
+"""
+# ----------------------------------------------------------------------------#
 # Controllers.
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
 
-@app.route('/')
+def create_playlist(form_data):
+    Memgraph.execute(f"CREATE (:TRACK {{artist_name: {form_data['ladida']}}})")
+
+
+@app.route("/get-tracks")
+def get_tracks():
+    results = memgraph.execute_and_fetch(f"MATCH (n:Track) RETURN n;")
+    tracks = [Track(result["n"]) for result in results]
+    return jsonify(tracks)
+
+
+@app.route("/get-top-tracks", defaults={"num_of_tracks": 10})
+@app.route("/get-top-tracks/<int:num_of_tracks>")
+def get_most_used_tracks(num_of_tracks):
+    results = memgraph.execute_and_fetch(
+        f"MATCH (n:Track)<-[r]-(m) RETURN n, COUNT(m) AS edg_count ORDER BY edg_count DESC LIMIT {num_of_tracks};"
+    )
+
+    tracks = [
+        {"track": Track(result["n"]), "num_of_playlists": result["edg_count"]}
+        for result in results
+    ]
+    return jsonify(tracks)
+
+
+@app.route("/get-top-playlists", defaults={"num_of_playlists": 10})
+@app.route("/get-top-playlists/<int:num_of_playlists>")
+def get_playlists_with_most_tracks(num_of_playlists):
+    results = memgraph.execute_and_fetch(
+        f"MATCH (n:Playlist)-[r]->(m) RETURN n, COUNT(m) AS edg_count ORDER BY edg_count DESC LIMIT {num_of_playlists};"
+    )
+    tracks = [
+        {"playlist": Playlist(result["n"]), "num_of_tracks": result["edg_count"]}
+        for result in results
+    ]
+    return jsonify(tracks)
+
+
+@app.route("/add-song", methods=["GET", "POST"])
+def add_playlist():
+    # "artist_name": "Degiheugi",
+    # "track_uri": "spotify:track:7vqa3sDmtEaVJ2gcvxtRID",
+    # "artist_uri": "spotify:artist:3V2paBXEoZIAhfZRJmo2jL",
+    # "track_name": "Finalement",
+    # "album_uri": "spotify:album:2KrRMJ9z7Xjoz1Az4O6UML",
+    # "duration_ms": 166264,
+    # "album_name": "Dancing Chords and Fireflies"
+    if request.method == "POST":
+        create_track(request.form)
+    else:
+        return show_the_login_form()
+
+
+@app.route("/")
 def home():
-    return render_template('pages/placeholder.home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-# Error handlers.
+    return render_template("pages/placeholder.home.html")
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    #db_session.rollback()
-    return render_template('errors/500.html'), 500
+    # db_session.rollback()
+    return render_template("errors/500.html"), 500
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+    return render_template("errors/404.html"), 404
+
 
 if not app.debug:
-    file_handler = FileHandler('error.log')
+    file_handler = FileHandler("error.log")
     file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
     )
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+    app.logger.info("errors")
 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # Launch.
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
 # Or specify port manually:
-'''
+"""
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
+"""
--- backend/models.py
+++ backend/models.py
@@ -1,31 +1,45 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String
-# from app import db
+from dataclasses import dataclass
 
-engine = create_engine('sqlite:///database.db', echo=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+@dataclass
+class Track:
+    artist_name: str
+    track_uri: str
+    artist_uri: str
+    track_name: str
+    album_uri: str
+    duration_ms: int
+    album_name: str
 
-# Set your classes here.
+    def __init__(self, data):
+        self.artist_name = data.properties["artist_name"]
+        self.track_uri = data.properties["track_uri"]
+        self.artist_uri = data.properties["artist_uri"]
+        self.track_name = data.properties["track_name"]
+        self.album_uri = data.properties["album_uri"]
+        self.duration_ms = data.properties["duration_ms"]
+        self.album_name = data.properties["album_name"]
 
-'''
-class User(Base):
-    __tablename__ = 'Users'
+@dataclass
+class Playlist:
+    name: str
+    collaborative: str
+    pid: str
+    modified_at: str
+    num_albums: str
+    num_tracks: str
+    num_followers: str
+    num_edits: str
+    duration_ms: str
+    num_artists: str
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(30))
-
-    def __init__(self, name=None, password=None):
-        self.name = name
-        self.password = password
-'''
-
-# Create tables.
-Base.metadata.create_all(bind=engine)
+    def __init__(self, data):
+        self.name = data.properties["name"]
+        self.collaborative = data.properties["collaborative"]
+        self.pid = data.properties["pid"]
+        self.modified_at = data.properties["modified_at"]
+        self.num_albums = data.properties["num_albums"]
+        self.num_tracks = data.properties["num_tracks"]
+        self.num_followers = data.properties["num_followers"]
+        self.num_edits = data.properties["num_edits"]
+        self.duration_ms = data.properties["duration_ms"]
+        self.num_artists = data.properties["num_artists"]
