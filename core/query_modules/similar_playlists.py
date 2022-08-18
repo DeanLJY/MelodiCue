54c8d25a2f1783f224db9184a0975a2a34aafaf5
Dean Leung <100091478+FalloutOne@users.noreply.github.com>
2022-08-19 02:42:48 +0800

    add similar playlists

--- core/query_modules/similar_playlists.py
+++ core/query_modules/similar_playlists.py
@@ -1,14 +1,41 @@
+from heapq import nlargest
+
 import mgp
 
 
 @mgp.read_proc
-def get(playlist_id: int) -> mgp.Record(playlist_ids=list[int]):
+def get(context: mgp.ProcCtx, playlist: mgp.Vertex) -> mgp.Record(playlists=list):
     """Returns a list of playlist_ids that are similar to the given playlist.
     Calculates similar playlists by calculating the proximity of each to the
     given one.
 
+    Example usage:
+        MATCH (playlist:Playlist)
+        WITH playlist
+        LIMIT 1
+        CALL similar_playlists.get(playlist) YIELD playlists
+        RETURN playlists
+
     :param int playlist_id: User playlist.
     :return: List of playlist ids that are currently trendy.
-    :rtype: mgp.Record(playlist_ids=list[int])
+    :rtype: mgp.Record(playlist_ids=list[dict[str][Any]])
     """
-    return mgp.Record(playlist_ids=[1, 2, 3])
+    tracks = set(map(lambda edge: edge.to_vertex.id, playlist.out_edges))
+    return mgp.Record(
+        playlists=list(
+            map(
+                lambda vertex: dict(vertex.properties),
+                nlargest(
+                    10,
+                    filter(
+                        lambda vertex: "Playlist" in vertex.labels,
+                        context.graph.vertices,
+                    ),
+                    key=lambda vertex: len(
+                        tracks
+                        & set(map(lambda edge: edge.to_vertex.id, vertex.out_edges))
+                    ),
+                ),
+            )
+        )
+    )
