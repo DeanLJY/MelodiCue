6f39651cbc430cdf8d9361f9317dfcee7eb3cab4
Dean Leung <100091478+FalloutOne@users.noreply.github.com>
2022-09-04 12:00:54 +0800

    Fix similar tracks query module StyleRemover

--- backend/app.py
+++ backend/app.py
@@ -61,9 +61,7 @@ def get_playlists_with_most_tracks(num_of_playlists):
             }
             for result in results
         ]
-        return jsonify(
-            {"playlists": playlists, "status": Status.SUCCESS, "message": ""}
-        )
+        return jsonify({"playlists": playlists, "status": Status.SUCCESS, "message": ""})
     except Exception as exp:
         return jsonify({"status": Status.FAILURE, "message": exp})
 
@@ -76,21 +74,15 @@ def add_track():
         track_uri = data["track_uri"]
 
         playlist_result = next(
-            memgraph.execute_and_fetch(
-                f"MATCH (n) WHERE ID(n) = {playlist_id} RETURN n;"
-            ),
+            memgraph.execute_and_fetch(f"MATCH (n) WHERE ID(n) = {playlist_id} RETURN n;"),
             None,
         )
         track_result = next(
-            memgraph.execute_and_fetch(
-                f"MATCH (n) WHERE n.track_uri = {to_cypher_value(track_uri)} RETURN n;"
-            ),
+            memgraph.execute_and_fetch(f"MATCH (n) WHERE n.track_uri = {to_cypher_value(track_uri)} RETURN n;"),
             None,
         )
 
-        playlist = (
-            Playlist.create_from_data(playlist_result["n"]) if playlist_result else None
-        )
+        playlist = Playlist.create_from_data(playlist_result["n"]) if playlist_result else None
         track = Track.create_from_data(track_result["n"]) if track_result else None
         if not playlist:
             return jsonify({"error": True, "message": "Playlist does not exist!"})
@@ -144,8 +136,7 @@ def create_playlist():
         name = to_cypher_value(data["name"])
         playlist = Playlist(name)
         result = memgraph.execute_and_fetch(
-            f"CREATE (n:{Playlist.LABEL} {{{playlist.to_cypher()}}}) RETURN id(n) as"
-            " playlist_id;"
+            f"CREATE (n:{Playlist.LABEL} {{{playlist.to_cypher()}}}) RETURN id(n) as" " playlist_id;"
         )
         playlist_id = next(result)["playlist_id"]
         return jsonify(
@@ -165,9 +156,7 @@ def rename_playlist():
         data = request.get_json()
         playlist_id = to_cypher_value(data["playlist_id"])
         name = to_cypher_value(data["name"])
-        memgraph.execute_and_fetch(
-            f"MATCH (n:{Playlist.LABEL}) WHERE id(n) = {playlist_id} SET n.name = {name};"
-        )
+        memgraph.execute_and_fetch(f"MATCH (n:{Playlist.LABEL}) WHERE id(n) = {playlist_id} SET n.name = {name};")
         return jsonify(
             {
                 "name": name,
@@ -185,20 +174,9 @@ def track_recommendation():
         data = request.get_json()
         playlist_id = data["playlist_id"]
         results = memgraph.execute_and_fetch(
-            f"MATCH (n: Playlist {{pid: {playlist_id}}})-[]->(track: Track), "
-            "(relevant_playlists: Playlist)-[]->(track) "
-            "WITH collect(track) as tracks, relevant_playlists "
-            "MATCH (relevant_playlists)-[]->(relevant_tracks: Track) "
-            "UNWIND tracks as t "
-            "WITH DISTINCT t, relevant_playlists, relevant_tracks "
-            "WITH collect(t) as tracks, collect(relevant_playlists) as relevant_playlists, relevant_tracks "
-            "UNWIND relevant_playlists as rp "
-            "WITH DISTINCT rp, tracks, relevant_tracks "
-            "WITH tracks, collect(rp) as relevant_playlists, collect(relevant_tracks) as relevant_tracks "
-            "UNWIND relevant_tracks as rt "
-            "WITH DISTINCT rt as relevant_tracks, tracks, relevant_playlists "
-            "WITH collect(relevant_tracks) as relevant_tracks, tracks, relevant_playlists "
-            "CALL similar_tracks.get(tracks, relevant_playlists, relevant_tracks) "
+            f"MATCH (n:Playlist {{pid: {playlist_id} }})-[]->(m:Track) "
+            "WITH COLLECT(m) AS tracks "
+            f"CALL similar_tracks.get_better({playlist_id}, tracks) "
             "YIELD result, score RETURN result, score ORDER BY score DESC LIMIT 10; "
         )
         tracks = [Track.create_from_data(result["result"]) for result in results]
@@ -231,9 +209,7 @@ def not_found_error(error):
 
 if not app.debug:
     file_handler = FileHandler("error.log")
-    file_handler.setFormatter(
-        Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
-    )
+    file_handler.setFormatter(Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
--- core/query_modules/similar_tracks.py
+++ core/query_modules/similar_tracks.py
@@ -5,25 +5,19 @@ import mgp
 
 
 def grade_playlist(relevant_playlist: mgp.Vertex, relevant_tracks: List[int]) -> int:
-    relevant_playlist_tracks = [
-        out_edge.to_vertex.id for out_edge in relevant_playlist.out_edges
-    ]
+    relevant_playlist_tracks = [out_edge.to_vertex.id for out_edge in relevant_playlist.out_edges]
     return len(list(set(relevant_playlist_tracks).intersection(relevant_tracks)))
 
 
 def grade_track(relevant_track: mgp.Vertex, graded_playlists: Dict[int, int]) -> int:
-    return sum(
-        graded_playlists.get(in_edge.from_vertex.id, 0)
-        for in_edge in relevant_track.in_edges
-    )
+    return sum(graded_playlists.get(in_edge.from_vertex.id, 0) for in_edge in relevant_track.in_edges)
 
 
 @mgp.read_proc
-def get(
+def get_better(
     context: mgp.ProcCtx,
-    tracks: mgp.List[mgp.Any],
-    relevant_playlists: List[mgp.Any],
-    relevant_tracks: List[mgp.Any],
+    playlist_id: int,
+    tracks: mgp.List[mgp.Vertex],
 ) -> mgp.Record(result=mgp.Vertex, score=int):
     """Returns a list of track_ids that are similar to the tracks in
     the given playlist. Calculates similar tracks by calculating the
@@ -31,44 +25,36 @@ def get(
 
     Cypher equivalent:
 
-    MATCH (n: Playlist {pid: 105637})-[]->(track: Track),
-    (relevant_playlists: Playlist)-[]->(track)
-    WITH collect(track) as tracks, relevant_playlists
-    MATCH (relevant_playlists)-[]->(relevant_tracks: Track)
-    UNWIND tracks as t
-    WITH DISTINCT t, relevant_playlists, relevant_tracks
-    WITH collect(t) as tracks, collect(relevant_playlists) as relevant_playlists, relevant_tracks
-    UNWIND relevant_playlists as rp
-    WITH DISTINCT rp, tracks, relevant_tracks
-    WITH tracks, collect(rp) as relevant_playlists, collect(relevant_tracks) as relevant_tracks
-    UNWIND relevant_tracks as rt
-    WITH DISTINCT rt as relevant_tracks, tracks, relevant_playlists
-    WITH collect(relevant_tracks) as relevant_tracks, tracks, relevant_playlists
-    CALL similar_tracks.get(tracks, relevant_playlists, relevant_tracks)
+    MATCH (n:Playlist {pid: $playlist_id})-[]->(m:Track)
+    WITH COLLECT(m) AS tracks
+    CALL similar_tracks.get_better($playlist_id, tracks)
     YIELD result, score RETURN result, score ORDER BY score DESC LIMIT 10;
     """
     track_ids = [track.id for track in tracks]
+    relevant_playlists = set()
+    for track in tracks:
+        for in_edge in track.in_edges:
+            if in_edge.from_vertex.id != playlist_id:
+                relevant_playlists.add(in_edge.from_vertex)
+
+    relevant_tracks = set()
+    for relevant_playlist in relevant_playlists:
+        for out_edge in relevant_playlist.out_edges:
+            if out_edge.to_vertex.id not in track_ids:
+                relevant_tracks.add(out_edge.to_vertex)
 
     # 1. Grade playlists according to how many relevant_tracks they have
     graded_playlists = dict()
     for relevant_playlist in relevant_playlists:
-        graded_playlists[relevant_playlist.id] = grade_playlist(
-            relevant_playlist, track_ids
-        )
+        graded_playlists[relevant_playlist.id] = grade_playlist(relevant_playlist, track_ids)
 
     # 2. Grade all tracks according to in how similar playlist they are
     recommended_tracks = dict()
     for relevant_track in relevant_tracks:
-        recommended_tracks[
-            grade_track(relevant_track, graded_playlists)
-        ] = relevant_track
-    sorted_recommended_tracks = OrderedDict(
-        sorted(recommended_tracks.items(), reverse=True)
-    )
+        recommended_tracks[grade_track(relevant_track, graded_playlists)] = relevant_track
+    sorted_recommended_tracks = OrderedDict(sorted(recommended_tracks.items(), reverse=True))
 
     scored_recommended_tracks = []
     for score, recommended_track in sorted_recommended_tracks.items():
-        scored_recommended_tracks.append(
-            mgp.Record(result=recommended_track, score=score)
-        )
+        scored_recommended_tracks.append(mgp.Record(result=recommended_track, score=score))
     return scored_recommended_tracks
